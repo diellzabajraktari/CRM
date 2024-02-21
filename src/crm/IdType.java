8eb8bd418871d6a2f4ba342b74828e14d8bb56e9
arnishox <arnis.hoxha@student.uni-pr.edu>
2024-02-22 00:55:18 +0100

    Update enum IdType

--- src/crm/IdType.java
+++ src/crm/IdType.java
@@ -1,7 +1,20 @@
 package crm;
 
 public enum IdType {
-    CU-Customer,
-    CO-Contract,
-    SU-Subscription
+    CU("Customer"),
+    CO("Contract"),
+    SU("Subscription");
+
+    private final String description;
+
+    IdType(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return description;
+    }
+
 }
---
+++ src/crm/SubscriptionService.java
@@ -0,0 +1,32 @@
+package crm;
+
+import java.util.List;
+
+public class SubscriptionService implements TelecomService<Subscription>{
+
+
+    @Override
+    public void create(Subscription entity) {
+
+    }
+
+    @Override
+    public void update(Subscription entity) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public Subscription findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Subscription> findAll() {
+        return null;
+    }
+}
